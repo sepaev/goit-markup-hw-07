76eff93870b2294f025dfc56b2d88768da42bc6d
sepaev <sepaev@gmail.com>
2021-03-15 01:38:40 +0200

    final change

--- script/modal.js
+++ script/modal.js
@@ -1,20 +1,3 @@
-// const handleBackdrop = name => {
-//   const backdrop = document.querySelector('[data-backdrop-${name}]');
-//   const buttons = document.querySelectorAll(
-//     '[data-toggle-backdrop-${name}]',
-    
-//   );
-//   const toggleBackdrop = () =>
-//     backdrop.classList.toogle('is-hidden');
-//   buttons.forEach(button =>
-//     button.addEventListener('click', toggleBackdrop),
-//   );
-// }
-
-//sign
-// handleBackdrop('sign')
-
-
 (() => {
   const refs = {
     openModalBtn: document.querySelector('[data-modal-open]'),
@@ -26,9 +9,9 @@
   refs.closeModalBtn.addEventListener('click', toggleModal);
 
   function toggleModal() {
-    refs.modal.classList.toggle('is-hidden');
+    refs.modal.classList.toggle('backdrop__is-hidden');
 
-    const isModalHidden = refs.modal.classList.contains('is-hidden');
+    const isModalHidden = refs.modal.classList.contains('backdrop__is-hidden');
     const method = isModalHidden ? "enableBodyScroll" : "disableBodyScroll";
     bodyScrollLock[method](refs.modal);
   }
